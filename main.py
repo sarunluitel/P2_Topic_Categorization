3511975ef3e41b5bd502af3aac136eaf16386016
Sarun Luitel <sarunluitel@unm.edu>
2018-10-17 17:45:30 -0600

    NaiveBayes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def main():
     print("Hello world")
     trainingData = pd.read_csv("data/testing.csv", nrows=5)
-    print(trainingData)
+    print(dir(plt))
 
 
 if __name__ == "__main__":
